09e690627461d79734029975066d8f709f0b71ed
Anuj <anuj1230567@gmail.com>
2025-07-12 09:34:44 +0530

    third change

--- project work/QR code code/basiccode.py
+++ project work/QR code code/basiccode.py
@@ -2,3 +2,4 @@ import qrcode as qr
 img= qr.make("https://search.brave.com/search?q=how+to+install+a+module+in+python+in+vscode&summary=1&conversation=2042ed0d71c1964f4d864c")
 img.save("module_qr.png")
 #just a comment
+#one more change
